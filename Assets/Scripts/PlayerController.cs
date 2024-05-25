ec2b4fb18483fcb7892532dec8442a788e4e7be8
luisesales <duduosales@hotmail.com>
2024-05-25 20:13:17 -0300

    attempting to add player movement script and finished Anthony animation configuration

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour   
+{
+    //PRIVATE
+    [SerializeField] private Animator animator; // Character Animator
+    private float speed; // Character Speed
+    private Vector2 movement; // Character Movement Direction
+    private Rigidbody2D rb;
+
+
+
+    //PUBLIC
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void UpdateMovement()
+    {        
+        /*
+         Animator Directions = { 
+        0 : Downwards,
+        1 : Leftside,
+        2 : Upwards,
+        3 : Rightside
+         */
+
+        float moveX = Input.GetAxisRaw("Horizontal"); // X input movement
+        float moveY = Input.GetAxisRaw("Vertical"); // Y input movement
+        movement = new Vector2(moveX,  moveY).normalized * Time.deltaTime * speed;
+
+        rb.SetRotation(0f);
+
+        // Animations prioritize the X movement
+
+        // Moving Upwards
+        if(moveY > 0)
+        {
+            animator.SetInteger("isMoving", 2);
+            animator.SetInteger("isIdle", -1);
+        }
+
+        // Moving Downwards
+        else if (moveY < 0)
+        {
+            animator.SetInteger("isMoving", 0);
+            animator.SetInteger("isIdle", -1);
+        }
+
+        // Idle 
+        else
+        {
+            animator.SetInteger("isMoving", -1);
+            if (animator.GetInteger("isMoving") != -1)
+                animator.SetInteger("isIdle", animator.GetInteger("isMoving"));
+            else
+                animator.SetInteger("isIdle", 0);
+        }
+
+        // Moving Rightside
+        if (moveX > 0)
+        {
+            rb.SetRotation(0f);
+            animator.SetInteger("isMoving", 3);
+            animator.SetInteger("isIdle", -1);
+
+
+        }
+
+        // Moving Leftside
+        else if (moveX < 0)
+        {
+            animator.SetInteger("isMoving", 1);
+            animator.SetInteger("isIdle", -1);
+        }
+
+        // Idle
+        else
+        {
+            animator.SetInteger("isMoving", -1);
+            if(animator.GetInteger("isMoving") != -1)
+                animator.SetInteger("isIdle", animator.GetInteger("isMoving"));
+            else
+                animator.SetInteger("isIdle", 0);
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+}
