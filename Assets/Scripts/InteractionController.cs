841c084195253bc7d6ce2a16e8e3cf8ad0ed0bbb
Bianca Miranda <miranda.bianca41@gmail.com>
2024-07-09 11:01:54 -0300

    Environment House

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class InteractionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite open;
+    public Sprite close;
+
+    private bool isOpen = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(!isOpen)
+            {
+                GetComponent<SpriteRenderer>().sprite = open;
+                isOpen = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = close;
+                isOpen = false;
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isOpen)
+            {
+                GetComponent<SpriteRenderer>().sprite = open;
+                isOpen = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = close;
+                isOpen = false;
+            }
+
+        }
     }
 }
